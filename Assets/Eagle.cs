df87e13f1a56b7bbbc50efd4ba23770c9dbd72fe
Nansae <orchidbird2@gmail.com>
2019-08-03 14:45:57 +0900

    A, D키로 좌우 이동

---
+++ Assets/Eagle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Eagle : MonoBehaviour {
+    public float speed = 3;
+    SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector3.left*Time.deltaTime*speed);
+            sprite.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector3.right*Time.deltaTime*speed);
+            sprite.flipX = true;
+        }
+    }
+}
