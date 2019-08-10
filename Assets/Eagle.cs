b515d41a44227dd2152e923409bd36a80c7f77f7
Nansae <orchidbird2@gmail.com>
2019-08-10 14:48:23 +0900

    Walk 애니메이션 작업 중

--- Assets/Eagle.cs
+++ Assets/Eagle.cs
@@ -10,32 +10,38 @@ public class Eagle : MonoBehaviour {
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    enum JumpState{None, Up, Down}
-    JumpState jumping;
+    enum MoveState{None, Jumping, Falling, Walking}
+    MoveState moveState;
     void Update() {
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Translate(Vector3.left*Time.deltaTime*speed);
-            sprite.flipX = false;
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(Vector3.right*Time.deltaTime*speed);
-            sprite.flipX = true;
-        }
+        if (Input.GetKey(KeyCode.A)) 
+            HorizontalMove(false);
+        if (Input.GetKey(KeyCode.D))
+            HorizontalMove(true);
 
         var animator = GetComponent<Animator>();
         var rigid = GetComponent<Rigidbody2D>(); 
-        if (jumping == JumpState.None && Input.GetKeyDown(KeyCode.Space)) {
+        if (moveState == MoveState.None && Input.GetKeyDown(KeyCode.Space)) {
             rigid.velocity = rigid.velocity + Vector2.up * jumpPower;
             animator.SetTrigger("Jump");
-            jumping = JumpState.Up;
+            moveState = MoveState.Jumping;
         }
 
-        if (jumping == JumpState.Up && rigid.velocity.y < 0)
-            jumping = JumpState.Down;
-        if (jumping != JumpState.None && rigid.velocity.y == 0) {
-            jumping = JumpState.None;
+        if (moveState == MoveState.Jumping && rigid.velocity.y < 0)
+            moveState = MoveState.Falling;
+        if (moveState != MoveState.None && rigid.velocity.y == 0) {
+            moveState = MoveState.None;
             animator.SetTrigger("Jump");
+        }
+        if(moveState == MoveState.Walking && rigid.velocity.x == 0)
+            animator.SetTrigger("Stop");
+    }
+
+    void HorizontalMove(bool right){
+        transform.Translate(right ? Vector3.right : Vector3.left *Time.deltaTime*speed);
+        sprite.flipX = right;
+        if (moveState == MoveState.None) {
+            moveState = MoveState.Walking;
+            GetComponent<Animator>().SetTrigger("Walk");
         }
     }
 }
